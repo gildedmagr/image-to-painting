18ec7a59fc79ce480c657d956fdab053e7b4b835
Andrii Protsenko <andrii.protsenko@masterofcode.com>
2021-12-28 21:30:04 +0200

    first commit

---
+++ src/main/java/ru/pechat55/Bootstrap.java
@@ -0,0 +1,22 @@
+package ru.pechat55;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.Spark;
+
+public class Bootstrap {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    public Bootstrap(){
+        Spark.port(8888);
+        Spark.staticFiles.location("/");
+        Spark.get("/", (request, response) -> {
+            Application.generateFiles();
+            response.body("Hello");
+            return "Hello World";
+        });
+    }
+
+    public static void start(){
+        new Bootstrap();
+    }
+}
