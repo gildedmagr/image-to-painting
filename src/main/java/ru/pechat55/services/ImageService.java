6135fe1d2b032b1017d87b0ea8f2c1d1d1cd2b8d
Andrii Protsenko <andrii.protsenko@masterofcode.com>
2022-01-05 21:40:52 +0200

    WIP

--- src/main/java/ru/pechat55/services/ImageService.java
+++ src/main/java/ru/pechat55/services/ImageService.java
@@ -109,7 +109,7 @@ public class ImageService {
         Imgproc.warpPerspective(image, warpImage3, warpMat, warpImage.size(), Imgproc.INTER_AREA, Core.BORDER_TRANSPARENT, new Scalar(255, 255, 255, 255));
         Imgproc.warpPerspective(image, warpImage4, warpMat, warpImage.size(), Imgproc.INTER_LANCZOS4, Core.BORDER_CONSTANT, new Scalar(255, 255, 255, 255));
 
-        Imgproc.line(warpImage, new Point(warpImage.width() - 1, 0), new Point(warpImage.width() - 1, warpImage.height()), new Scalar(155, 155, 155, 100), 1);
+        Imgproc.line(warpImage, new Point(warpImage.width() - 1, 0), new Point(warpImage.width() - 1, warpImage.height()), new Scalar(0, 0, 0, 0), 1);
         return warpImage;
     }
 
