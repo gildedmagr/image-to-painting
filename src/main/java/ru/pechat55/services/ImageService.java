30468093b7ad0aab77877457b8d6cb893614e727
Andrii Protsenko <andrii.protsenko@masterofcode.com>
2022-01-05 21:35:13 +0200

    WIP

--- src/main/java/ru/pechat55/services/ImageService.java
+++ src/main/java/ru/pechat55/services/ImageService.java
@@ -331,7 +331,7 @@ public class ImageService {
         // create the new image, canvas size is the max. of both image sizes
         int w = Math.max(image.getWidth(), border.getWidth());
         int h = Math.max(image.getHeight(), border.getHeight());
-        BufferedImage combined = new BufferedImage(w+50, h+50, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage combined = new BufferedImage(w + border.getWidth(), h, BufferedImage.TYPE_INT_ARGB);
 
         // paint both images, preserving the alpha channels
         Graphics combinedGraphics = combined.createGraphics();
